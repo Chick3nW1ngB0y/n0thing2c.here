506cfa6bc6f2229c45b334f307fb45b405f23f98
R3ficulSolom0n <refisalman2806@gmail.com>
2025-04-16 07:18:16 +0700

    Delete UKK Kasir kelompok Jo directory

--- UKK Kasir kelompok Jo/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","ukk_kasir_jo");
-
-// Check connection
-if (mysqli_connect_error()){
-    echo "koneksi database gagal : " . mysqli_connect_error();
-}
-
-?>
